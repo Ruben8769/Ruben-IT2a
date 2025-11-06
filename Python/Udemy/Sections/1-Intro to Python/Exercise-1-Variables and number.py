cf921c68b90904d57ad18a9ea7cb81206e6695ce
Ruben8769 <ruhvidsten@outlook.com>
2025-11-06 18:28:28 +0100

    Removed Udemy from folder

--- Python/Udemy/Sections/1-Intro to Python/Exercise-1-Variables and number.py
+++
@@ -1,9 +0,0 @@
-# Create two variables, var1 and var2, both with the same value.
-
-# Create two variables, num1 and num2, which multiply together to give 16.
-
-var1 = 10
-var2 = 10
-
-num1 = 2
-num2 = 8
--- Python/Udemy/Sections/1-Intro to Python/Exercise-2-Communicating with users.py
+++
@@ -1,6 +0,0 @@
-user_name = input("What's your name? ")
-print(f"Hello, {user_name}")
-
-user_age = int(input("What's your age? "))
-user_age = user_age * 12
-print(user_age)
