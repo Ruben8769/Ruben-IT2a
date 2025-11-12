ddad4f3a4005f1c05ca3ff09e0e11df2c53e91e6
Ruben <136503978+Ruben8769@users.noreply.github.com>
2025-11-12 09:38:50 +0100

    Added code for encryption

--- Python/Encryption-Decryption-Test/test.py
+++ Python/Encryption-Decryption-Test/test.py
@@ -1,22 +1,35 @@
 import json
 
-with open("Python/Encryption-Decryption-Test/Legal-ch.json", "r") as lch: legal_chs = json.load(lch)
+with open("Python/Encryption-Decryption-Test/Legal-ch.json", "r") as lch:
+    legal_chs = json.load(lch)
 
-user_password = input("Password: ")
-user_password_list = list(user_password) # Sjeker om passored bruker lovelig tegn
+encryption_key = 5
+user_password = list(input("Password: "))
 
+# Sjeker om passored bruker lovelig tegn
 allowed_ch = []
-for user_ch in user_password_list:
+for user_ch in user_password:
     for legal_ch in legal_chs:
         if user_ch == legal_ch:
             allowed_ch.append(True)
             break
     else: allowed_ch.append(False)
 
-print(allowed_ch)
-
+# Koden som encrypterer bruker passored
 for character in allowed_ch:
     if character == False:
         break
 else:
-    pass
+    position = []
+    new_postion = []
+    encryptet_chars = []
+    for us_ch in user_password:
+        index_value = legal_chs.index(us_ch)
+        position.append(index_value)
+    for pos in position:
+        new_pos = (pos + encryption_key) % len(legal_chs)
+        new_postion.append(new_pos)
+        encryptet_chars.append(legal_chs[new_pos])
+    encryptet_passwords = "".join(encryptet_chars)
+
+# Koden som decrypterer bruker passored
